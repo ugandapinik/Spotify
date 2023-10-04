f8843541109e2b4f5f7b10d3b0d1f2ce31b9304a
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2023-10-04 13:49:45 -0230

    Your library included

--- components/Library.tsx
+++ components/Library.tsx
@@ -12,12 +12,18 @@ const Library = () => {
     }
 
     return (
-        <div className="flex items-center justify-between px-5 pt-4">
-            <div className="inline-flex items-center gap-x-2">
-                <TbPlaylist className="text-neutral-400" size={26} />
-                <p className="text-neutral-400 font-medium text-md">Your Library</p>
+        <div className="flex flex-col" >
+            <div className="flex items-center justify-between px-5 pt-4">
+                <div className="inline-flex items-center gap-x-2">
+                    <TbPlaylist className="text-neutral-400" size={26} />
+                    <p className="text-neutral-400 font-medium text-md">Your Library</p>
+                </div>
+                <AiOutlinePlus onClick={onClick} size={20} className="text-neutral-400 cursor-pointer hover:text-white transition" />
             </div>
-            <AiOutlinePlus onClick={onClick} size={20} className="text-neutral-400 cursor-pointer hover:text-white transition" />
+
+            <div className="flex flex-col gap-y-2 mt-4 px-3">
+                List of Songs!
+            </div>
         </div>
     )
 }
