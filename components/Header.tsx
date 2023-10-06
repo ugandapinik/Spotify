81a721299538859bb89076c6bee520c9964aba1a
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2023-10-06 14:44:12 -0230

    Header: home icons included

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge"
-import { RxCaretLeft } from "react-icons/rx"
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
+import { HiHome } from "react-icons/hi"
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -26,9 +27,16 @@ const Header: React.FC<HeaderProps> = ({
         <div className={twMerge(`h-fit bg-gradient-to-b from-emerald-800 p-6`, className)}>
             <div className="w-full mb-4 flex items-center justify-between">
                 <div className="hidden md:flex gap-x-2 items-center">
-                    <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                    <button onClick={() => router.back()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
                         <RxCaretLeft className="text-white" size={35} />
                     </button>
+                    <button onClick={() => router.forward()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                        <RxCaretRight className="text-white" size={35} />
+                    </button>
+                </div>
+
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <HiHome />
                 </div>
             </div>
         </div>
