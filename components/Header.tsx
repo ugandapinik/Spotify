56e1d5fbcaaa638880b9c63a9e99075d322cf75f
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2023-10-07 22:34:42 -0230

    Header: include home and search icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge"
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
 import { HiHome } from "react-icons/hi"
+import { BiSearch } from "react-icons/bi"
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -36,7 +37,12 @@ const Header: React.FC<HeaderProps> = ({
                 </div>
 
                 <div className="flex md:hidden gap-x-2 items-center">
-                    <HiHome />
+                    <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <HiHome className="text-black" size={20} />
+                    </button>
+                    <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <BiSearch className="text-black" size={20} />
+                    </button>
                 </div>
             </div>
         </div>
