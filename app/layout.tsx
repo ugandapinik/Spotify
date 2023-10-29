7cf752e092071a8795d25f6ad67fb6a3a9960dff
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2023-10-29 13:46:52 -0230

    Your library included

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={figtree.className}>
+            <body suppressHydrationWarning={true} className={figtree.className}>
                 <Siderbar>
                     {children}
                 </Siderbar>
