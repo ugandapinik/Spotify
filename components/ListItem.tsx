a0a9bd0d6a84eca82ab59e465b93fc51371ff078
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2023-11-15 23:39:48 -0330

    ListItem: include the design for the list item

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,5 +1,6 @@
 "use client"
-
+import Image from "next/image"
+import { FaPlay } from "react-icons/fa"
 import { useRouter } from "next/navigation"
 
 interface ListItemProps {
@@ -14,10 +15,25 @@ const ListItem: React.FC<ListItemProps> = ({
     href
 }) => {
     const router = useRouter()
-    return (
-        <div>
 
-        </div>
+    const onClick = () => {
+        // Authenticated befor push
+        router.push(href)
+    }
+
+    return (
+        <button onClick={onClick} className="relative group flex items-center rounded-md overflow-hidden gap-x-4
+                        bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+
+            <div className="relative min-h-[64px] min-w-[64px]">
+                <Image fill={true} className="object-cover" src={image} alt="Image" />
+            </div>
+            <p className="font-medium truncate py-5">{name}</p>
+            <div className="absolute transition opacity-1 rounded-full flex items-center opacity-0 
+                            justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+                <FaPlay className="text-black" />
+            </div>
+        </button>
     )
 }
 
